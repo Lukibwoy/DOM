b63193fbb03afd8f5555aaeb0a94b493f89be163
Lukibwoy <lukaszwiczynski@gmail.com>
2023-04-10 23:40:30 +0200

    update2

--- js/script.js
+++ js/script.js
@@ -88,13 +88,21 @@ console.log(fourthP.textContent)
 
 //     Change style of each paragraph using JavaScript(eg. color, background, border, font-size, font-family)
 
-for(let i=0; i < allP.length; i++){
-allP[i].style.color = 'red'
-allP[i].style.background = 'blue'
-allP[i].style.border = 'black'
-allP[i].style.fontSize = '20px'
-allP[i].style.fontFamily = 'Arial'
-}
+// for(let i=0; i < allP.length; i++){
+// allP[i].style.color = 'red'
+// allP[i].style.background = 'blue'
+// allP[i].style.border = 'black'
+// allP[i].style.fontSize = '20px'
+// allP[i].style.fontFamily = 'Arial'
+// }
+allP.forEach(function (p) {
+	p.style.color = 'red'
+	p.style.background = 'blue'
+	p.style.border = 'black'
+	p.style.fontSize = '20px'
+	p.style.fontFamily = 'Arial'
+})
+
 //     Select all paragraphs and loop through each elements and give the first and third paragraph a color of green, and the second and the fourth paragraph a red color
 allP.forEach((p, index) => {
 	if (index % 2 === 0) {
@@ -105,13 +113,83 @@ allP.forEach((p, index) => {
 })
 //     Set text content, id and class to each paragraph
 
-
 // Exercise: Level 3
 // DOM: Mini project 1
 
 //     Develop the following application, use the following HTML elements to get started with. You will get the same code on starter folder. Apply all the styles and functionality using JavaScript only.
 //         The year color is changing every 1 second
+
+function changeColor2() {
+	setInterval(() => {
+		const randomColor2 = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
+			Math.random() * 256
+		)})`	
+		let h1 = document.querySelector('h1')
+		let content = h1.innerHTML
+		let newContent = content.replace('2020', `<span style="color:${randomColor2}">2020</span>`)
+
+	h1.innerHTML = newContent
+  }, 1000)
+}
+
+changeColor2()
+
+const allLi = document.querySelectorAll('.wrapper ul li')
+
 //         The date and time background color is changing every on seconds
 //         Completed challenge has background green
+const completed = document.querySelector('.wrapper ul li')
+completed.style.backgroundColor = 'green'
 //         Ongoing challenge has background yellow
+
+allLi.forEach(item => {
+	if (item.textContent.includes('Ongoing')) {
+		item.style.backgroundColor = 'yellow'
+	}
+})
 //         Coming challenges have background red
+
+allLi.forEach(item => {
+	if (item.textContent.includes('Coming')) {
+		item.style.backgroundColor = 'red'
+	}
+})
+
+// Exercies 5
+
+/*
+  Exercise 01
+  -----------
+  Highlight all of the words over 8 characters long in the paragraph text (with a yellow background for example)
+*/
+
+const heading = document.querySelector('.heading')
+const words = heading.textContent.split(' ')
+for (let i = 0; i < words.length; i++) {
+	if (words[i].length > 8) {
+		words[i] = '<span style="background-color: yellow;">' + words[i] + '</span>'
+	}
+}
+heading.innerHTML = words.join(' ')
+/*
+  Exercise 02
+  -----------
+  Add a link back to the source of the text after the paragraph tag.
+  (https://forcemipsum.com/)*/
+
+const link = document.createElement('a')
+link.href = 'https://forcemipsum.com/'
+link.innerText = 'Link'
+heading.appendChild(link)
+
+/*
+  Exercise 03
+  -----------
+  Split each new sentence on to a separate line in the paragraph text.
+  A sentence can be assumed to be a string of text terminated with a period (.)
+*/
+
+const sentences = heading.textContent.split('. ')
+for (let i = 0; i < sentences.length; i++) {
+	console.log(sentences[i])
+}
